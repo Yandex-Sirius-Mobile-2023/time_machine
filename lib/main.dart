964026bd77acfc38fb0998819a7373cc2e526adb
vladcto <55505918+vladcto@users.noreply.github.com>
2023-04-14 14:42:44 +0300

    Logger package configured.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:time_machine/core/utils/logging.dart.dart';
 
 import 'app.dart';
 
 void main() {
+  initLogger();
   runApp(const App());
 }
