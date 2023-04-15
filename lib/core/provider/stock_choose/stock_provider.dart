39129ed4d62801ebe0af5e5cf79714352245f5b6
igo320345 <igo320341@gmail.com>
2023-04-15 18:46:27 +0300

    add image loading to stock list

--- lib/core/provider/stock_choose/stock_provider.dart
+++ lib/core/provider/stock_choose/stock_provider.dart
@@ -1,7 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:time_machine/core/provider/stock_choose/stock_chooser.dart';
+import 'package:time_machine/data/env.dart';
 import 'package:time_machine/data/models/stock.dart';
 import 'package:time_machine/data/services/stock_service_impl.dart';
+import 'package:time_machine/data/stocks_api/finhub_api.dart';
 
 final stockChooseProvider =
     StateNotifierProvider<StockChooser, Map<StockTicker, bool>>(
@@ -15,4 +18,16 @@ final stockCostProvider = FutureProvider<Map<StockTicker, double>>((ref) async {
     stockCosts[stock.ticker] = stock.quotesHistory.values.toList()[0];
   }
   return stockCosts;
+});
+
+final stockImageProvider =
+    FutureProvider<Map<StockTicker, String>>((ref) async {
+  final service = FinhubApi(Env.finhubKey, Dio());
+  Map<StockTicker, String> stockImages = {};
+  for (StockTicker ticker in StockTicker.values) {
+    final stockDto = await service
+        .fetchStock(ticker.toString().split('.').last.replaceAll('_', '.'));
+    stockImages[ticker] = stockDto.logo;
+  }
+  return stockImages;
 });
