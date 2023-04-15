598d9b5d30926d499136335d05d2de4c6a1f4be5
igo320345 <igo320341@gmail.com>
2023-04-15 10:59:00 +0300

    fix

--- packages/analytics/lib/analytics.dart
+++ packages/analytics/lib/analytics.dart
@@ -50,7 +50,7 @@ class Analytics {
       final quotes = quotesHistory.values.toList();
       mat.add(quotes);
     }
-    mat = transformLogReturn(transpose(mat));
+    mat = _transformLogReturn(_transpose(mat));
 
     List<double> weight = [];
     int n = mat[0].length;
@@ -62,11 +62,12 @@ class Analytics {
     double quantile = -1.6448536269514729;
 
     for (int i = 0; i < n; i++) {
-      mu[i] = mean(getColumn(mat, i));
-      res[i + 1] = -mu[i] - sqrt(variance(getColumn(mat, i), mu[i])) * quantile;
+      mu[i] = _mean(_getColumn(mat, i));
+      res[i + 1] =
+          -mu[i] - sqrt(_variance(_getColumn(mat, i), mu[i])) * quantile;
     }
 
-    List<List<double>> covMat = covMatrix(mat, mu);
+    List<List<double>> covMat = _covMatrix(mat, mu);
     double weightXmu = 0;
     double m2 = 0;
 
@@ -124,7 +125,7 @@ class Analytics {
 }
 
 extension AnalyticsMathExtension on Analytics {
-  List<List<double>> transpose(List<List<double>> data) {
+  List<List<double>> _transpose(List<List<double>> data) {
     int rows = data.length;
     int cols = data[0].length;
     List<List<double>> res = List.generate(cols, (i) => List.filled(rows, 0));
@@ -136,12 +137,12 @@ extension AnalyticsMathExtension on Analytics {
     return res;
   }
 
-  List<List<double>> transformLogReturn(List<List<double>> data) {
+  List<List<double>> _transformLogReturn(List<List<double>> data) {
     int n = data[0].length;
     int countRows = data.length;
     List<List<double>> res = List.generate(n, (i) => List.filled(countRows, 0));
     for (int k = 0; k < n; k++) {
-      List<double> z1 = getColumn(data, k);
+      List<double> z1 = _getColumn(data, k);
       List<double> z2 = z1.sublist(0, countRows - 1);
       z2.insert(0, z1[0]);
       List<double> ret = [];
@@ -150,10 +151,10 @@ extension AnalyticsMathExtension on Analytics {
       }
       res[k] = ret;
     }
-    return transpose(res);
+    return _transpose(res);
   }
 
-  double mean(List<double> arr) {
+  double _mean(List<double> arr) {
     int n = arr.length;
     double res = 0;
     for (int i = 0; i < n; i++) {
@@ -162,7 +163,7 @@ extension AnalyticsMathExtension on Analytics {
     return res / n;
   }
 
-  double variance(List<double> arr, double avg) {
+  double _variance(List<double> arr, double avg) {
     int n = arr.length;
     double res = 0;
     for (int i = 0; i < n; i++) {
@@ -171,7 +172,7 @@ extension AnalyticsMathExtension on Analytics {
     return res / (n - 1);
   }
 
-  double covariance(
+  double _covariance(
       List<double> arr1, List<double> arr2, double avg1, double avg2) {
     int n = arr1.length;
     double cov = 0;
@@ -181,7 +182,7 @@ extension AnalyticsMathExtension on Analytics {
     return cov / (n - 1);
   }
 
-  List<double> getColumn(List<List<double>> matrix, int index) {
+  List<double> _getColumn(List<List<double>> matrix, int index) {
     List<double> column = [];
     for (int i = 0; i < matrix.length; i++) {
       column.add(matrix[i][index]);
@@ -189,13 +190,13 @@ extension AnalyticsMathExtension on Analytics {
     return column;
   }
 
-  List<List<double>> covMatrix(List<List<double>> mat, List<double> mu) {
+  List<List<double>> _covMatrix(List<List<double>> mat, List<double> mu) {
     int n = mat[0].length;
     List<List<double>> res = List.generate(n, (_) => List.filled(n, 0));
     for (int i = 0; i < n; i++) {
       for (int j = i; j < n; j++) {
         res[i][j] =
-            covariance(getColumn(mat, i), getColumn(mat, j), mu[i], mu[j]);
+            _covariance(_getColumn(mat, i), _getColumn(mat, j), mu[i], mu[j]);
       }
     }
     for (int i = 0; i < n; i++) {
