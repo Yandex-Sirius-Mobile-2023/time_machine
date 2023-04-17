f949e1f007132628ded4eb830fcf55ebb284b913
Timur Kharin <timurx404@gmail.com>
2023-04-17 12:19:17 +0300

    states of portfolio

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:time_machine/ui/portfoliopage.dart';
+import 'package:time_machine/ui/pages/portfoliopage.dart';
 import 'package:time_machine/uikit/ui_theme.dart';
 
 class App extends StatelessWidget {
@@ -11,7 +11,6 @@ class App extends StatelessWidget {
       theme: ThemeData(
         colorScheme: darkScheme,
       ),
-      home: const PortfolioPage(),
     );
   }
 }
--- lib/ui/widgets/consumer_portfolio.dart
+++ lib/ui/widgets/consumer_portfolio.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:time_machine/core/operations.dart';
 import 'package:time_machine/data/models/portfolio.dart';
-import 'package:time_machine/data/services/stock_service.dart';
-import 'package:time_machine/data/services/stock_service_impl.dart';
-
-import '../../data/models/stock.dart';
 
 class PortfolioWidget extends ConsumerWidget {
   final Portfolio portfolio;
@@ -18,7 +14,7 @@ class PortfolioWidget extends ConsumerWidget {
     return Center(
       child: Column(
         children: [
-          Text('Portfolio: ${state.portfolio.balance}'),
+          PortfolioWidget(portfolio: portfolio)
         ],
       ),
     );
