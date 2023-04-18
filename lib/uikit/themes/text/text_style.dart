aeec211535ceddbbd487bffaa326ab3f7800f59d
vladcto <55505918+vladcto@users.noreply.github.com>
2023-04-18 14:09:54 +0300

    Обновление темы

---
+++ lib/uikit/themes/text/text_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:time_machine/uikit/themes/ui_colors.dart';
+
+const TextStyle infoStyle = TextStyle(
+  color: UIColors.darkGrey,
+  fontSize: 14,
+);
