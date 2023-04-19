3fb8d7146e9546be3cfc1cbd391680bb334e41dd
vladcto <55505918+vladcto@users.noreply.github.com>
2023-04-19 21:43:34 +0300

    Подключил крашлитику

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:time_machine/core/utils/logging.dart.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
