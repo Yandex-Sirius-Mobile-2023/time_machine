8bed451d8296f9c3e818ba0d4a5089c88d9923d4
igo320345 <igo320341@gmail.com>
2023-04-19 09:42:53 +0300

    delete injection.dart

--- lib/injection.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:time_machine/data/services/stock_service.dart';
-import 'package:time_machine/data/services/stock_service_impl.dart';
-
-final stockServiceProvider = Provider<StockService>(
-  (ref) => StockServiceImpl(),
-);
